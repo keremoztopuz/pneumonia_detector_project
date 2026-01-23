15791d98bc7937fcab7547df55dbebc290fc9efa
keremozt <keremoztopuzzz@gmail.com>
2026-01-23 17:55:14 +0300

    vertical flip and random rotation removed

--- src/dataset.py
+++ src/dataset.py
@@ -8,8 +8,6 @@ from src import config
 train_transforms = transforms.Compose([
     transforms.Resize((config.IMAGE_SIZE, config.IMAGE_SIZE)),
     transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomVerticalFlip(p=0.2),
-    transforms.RandomRotation(30),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
     transforms.RandomAffine(degrees=15, translate=(0.15, 0.15), scale=(0.9, 1.1)),
     transforms.RandomPerspective(distortion_scale=0.2, p=0.3),
